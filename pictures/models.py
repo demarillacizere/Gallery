079fb349524c5f5844a51766ccd943fcaaa17513
Demarillac Izere <izered3@gmail.com>
2020-12-17 07:29:51 +0200

    created a location model

--- pictures/models.py
+++ pictures/models.py
@@ -22,6 +22,32 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+class Location(models.Model):
+    name = models.CharField(max_length =50)
+
+    @classmethod
+    def tag_articles(cls):
+        tags = cls.objects.all()
+        return tags
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    def update_location(self, update):
+        self.name = update
+        self.save()
+
+    @classmethod
+    def get_location_id(cls, id):
+        locate = Location.objects.get(pk = id)
+        return locate
+
+    def __str__(self):
+        return self.name
+
 class Image(models.Model):
     image = models.ImageField(upload_to = 'pictures/')
     image_name = models.CharField(max_length =30)
