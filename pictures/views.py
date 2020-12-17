b45dc87740f79d206d6de786065acb752d1c3ec9
Demarillac Izere <izered3@gmail.com>
2020-12-17 07:35:16 +0200

    created the first view

--- pictures/views.py
+++ pictures/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http  import HttpResponse,Http404
+import datetime as dt
+from .models import Image, Location, Category
 
-# Create your views here.
+def gallery(request):
+    pictures = Article.query.all()
+    return render(request, 'index.html', {"pictures":pictures})
+
