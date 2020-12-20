1ebfa3a9b7228c09f743695811945d20b7e7a492
Demarillac Izere <izered3@gmail.com>
2020-12-20 15:39:54 +0200

    updated the tests

--- pictures/urls.py
+++ pictures/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url(r'^$',views.gallery,name='gallery'),
+     url(r'^search/', views.search_image, name='search_image'),
     url(r'^image/(?P<category_name>\w+)/(?P<image_id>\d+)',views.picture,name = 'picture')
 ]
 if settings.DEBUG:
